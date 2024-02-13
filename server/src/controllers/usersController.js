395faf6f14976fa1a5b6e288da05a585f39702d5
Thinkedou <r.enguehard@gmail.com>
2024-02-13 11:40:05 +0100

    create user raw without encrypt pwd

--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -8,8 +8,15 @@ const exposeController = {
         return res.json(allUsers)
     },
     createUser:async (req,res)=>{
-        const {body} = req 
-        return res.json(body)
+        const {body}  = req
+        try {
+                const registeredUser = await usersService.createUser(body)     
+                return res.json(registeredUser)
+            } catch (error) {
+               return res.sendStatus(400)
+            // return res.json({error})
+        }
+        
     }
 
 
--- server/src/services/usersService.js
+++ server/src/services/usersService.js
@@ -1,13 +1,22 @@
-import userModel from "#src/models/Users";
+import User from "#src/models/Users";
 
 const exposeServices = {
 
     findAllUsers: async ()=>{
         try {
-            const   allUsers = await userModel.find({})
+            const   allUsers = await User.find({})
             return  allUsers
         } catch (error) {
-            throw error(error)
+            throw new Error(error)
+        }
+    },
+    createUser: async (rawData)=>{
+        try {
+            const   toSave  = new User(rawData)
+            const   newUser = toSave.save()   
+            return  newUser
+        } catch (error) {
+            throw new Error(error)
         }
     }
 
